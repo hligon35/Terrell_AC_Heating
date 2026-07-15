90a76b90b0773c74b5ef7eb499a5c6b9d5d4f756
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-15 18:45:31 -0400

    Add footer to app layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Footer from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex-1">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </div>
+  )
 }
